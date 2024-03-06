53e40173fad634477f13d64780680619dd9f7e91
Brayan Urena <brayanurena@Brayans-MacBook-Air.local>
2024-03-06 11:01:41 -0500

    Implement task rendering

--- script.js
+++ script.js
@@ -25,9 +25,9 @@ const taskTable = document.getElementById("taskTable");
     const deadLine = document.getElementById('taskDeadline').value;
 
     // TODO: Validate input fields
-    if(taskName && deadLine === 0){
-        alert('Task name and deadline are needed!')
-        return(handSubmission);
+    if(taskName.value === '' && deadLine.value === ''){
+        alert('Task name and deadline are needed!');
+        return;
     }
 
     // TODO: Update the tasks array
@@ -35,8 +35,17 @@ const taskTable = document.getElementById("taskTable");
 
     render();
  }
- const render = () =>{
+ function render(){
     // TODO: Use array methods to create a new table row of data for each item in the array
+    taskTable.innerHTML = tasks.map(task => `
+    <tr>
+        <td>${task.name}</td>
+        <td>${task.description}</td>
+        <td>${task.deadline}</td>
+        <td><button onclick="markTaskComplete(this)">Complete</button></td>
+        <td><button onclick="removeTask(this)">Remove</button></td>
+    </tr>
+`).join('');
  }
  
  function init(){
@@ -46,4 +55,4 @@ const taskTable = document.getElementById("taskTable");
  }
 
 
- console.log(handSubmission);
+ 
